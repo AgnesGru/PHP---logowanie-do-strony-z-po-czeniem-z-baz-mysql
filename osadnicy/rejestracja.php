cdd0844ebbcc09b3cc86b975744c46f41ae0d7f8
Agnes Gruszecka <agnieszka.gruszecka@gmail.com>
2020-07-20 11:25:55 +0200

    dodane hashowanie hasla, porównywanie hasla1 i hasla 2, sprawdzanie czy mail ma odpowiednią formule

--- osadnicy/rejestracja.php
+++ osadnicy/rejestracja.php
@@ -14,9 +14,63 @@
 		if ((strlen($nick)<3) || (strlen($nick)>30))
 		{
 			$wszystko_ok = false;
-			$_SESSION['e_nick']='Nick musi posiadać od trzech do 30 znaków';		
+			$_SESSION['e_nick']='Nick musi posiadać od trzech do 30 znaków';
+		}
+		
+		if(ctype_alnum($nick)==false)
+		{
+			$wszystko_ok = false;
+			$_SESSION['e_nick']='Nick może składać się tylko z liter (ang alfabet) i cyfr';
+		}
+		
+		// sprawdz poprawność e-mail		
+		$email = $_POST['email'];
+		$emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+		
+		if ((filter_var($emailB, FILTER_VALIDATE_EMAIL) == false) || ($emailB!=$email))
+		{
+			$wszystko_ok = false;
+			$_SESSION['e_email'] = 'Podaj poprawny adres email';
 			
 		}
+			
+		// Sprawdz poprawnoć hasła
+		
+		$haslo1 = $_POST['haslo1'];
+		$haslo2 = $_POST['haslo2'];
+		
+		# najpierw pobralismy hasło z formularza
+		
+		// sptawdzmy dlugosci hasło
+		
+		if ((strlen($haslo1) <8) || (strlen($haslo1)>20))
+		{
+			$wszystko_ok = false;
+			$_SESSION['e_haslo'] = 'Hasło  musi mieć od 8 do 20 znaków';			
+			
+		}
+		
+		if ($haslo1 != $haslo2)
+		{
+			$wszystko_ok = false;
+			$_SESSION['e_haslo'] = 'Hasła nie są identyczne';				
+		}	
+		
+		$haslo_hash = password_hash($haslo1, PASSWORD_DEFAULT);
+		# to wyświetla nam shaszowane hasla
+		# echo $haslo_hash; exit();		
+		
+		// cz regulamin został zaakceptowany?
+		
+		if (!isset($_POST['regulamin']))
+		{
+			$wszystko_ok = false;
+			$_SESSION['e_regulamin'] = 'Zaakceptuj regulamin';				
+		}
+		
+		// Sprawdz CAPTcha
+		$sekret = 
+		
 		
 		if ($wszystko_ok ==true)
 		{
@@ -24,8 +78,7 @@
 			echo 'Udana walidacja!'; 
 			exit();
 		}
-		
-		
+				
 	}
 	
 ?>
@@ -68,11 +121,48 @@
 		?>
 		
 		E - mail: <br/> <input type = "text" name = "email"/><br/>
+		
+		<?php
+		
+			if (isset($_SESSION['e_email']))
+			{
+				
+				echo '<div class = "error">'.$_SESSION['e_email'].'</div>';
+				unset($_SESSION['e_email']);
+				
+			}
+		
+		?>
+		
 		Twoje hasło: <br/> <input type = "password" name = "haslo1"/><br/>
+		
+		<?php
+		
+			if (isset($_SESSION['e_haslo']))
+			{
+				
+				echo '<div class = "error">'.$_SESSION['e_haslo'].'</div>';
+				unset($_SESSION['e_haslo']);
+				
+			}
+		
+		?>
 		Powtórz hasło: <br/> <input type = "password" name = "haslo2"/><br/>
+		
 		<label>
 		<input type = "checkbox" name = "regulamin"/>Akceptuje regulamin<br/>
 		</label>
+		<?php
+		
+			if (isset($_SESSION['e_regulamin']))
+			{
+				
+				echo '<div class = "error">'.$_SESSION['e_regulamin'].'</div>';
+				unset($_SESSION['e_regulamin']);
+				
+			}
+		
+		?>
 		<div class = "g-recaptcha" data-sitekey="6LeWgrIZAAAAAJZ4XmWBeW4RpTSRFddiDQVwhKCM"></div>
 		<br/>
 		<input type = "submit" value = "Zarejestruj się"/>
--- osadnicy/zaloguj.php
+++ osadnicy/zaloguj.php
@@ -26,38 +26,47 @@
 		$haslo = $_POST['haslo'];
 		
 		$login = htmlentities($login, ENT_QUOTES, "UTF-8");
-		$login = htmlentities($haslo, ENT_QUOTES, "UTF-8");
+		
 		
 		# zapytnie które sprawdza czy uzytkownik istnieje
 		# całe zapytanie w " " a zmienne w ' ' inaczej niż w echo
 		
 		# wysyłamy zapytanie do bazy danych i zabezpieczenie przed wstrzykiwaniem sql
 		if ($rezultat = @$polaczenie->query(
-		sprintf("SELECT * FROM uzytkownicy WHERE user='%s' AND pass='%s'",
+		sprintf("SELECT * FROM uzytkownicy WHERE user='%s'",
 		mysqli_real_escape_string($polaczenie, $login),
 		mysqli_real_escape_string($polaczenie, $haslo))))
 		{
 			$ilu_userow = $rezultat->num_rows;
 			if ($ilu_userow>0)
 			{
-				$_SESSION['zalogowany'] = true;
-				
 				$wiersz = $rezultat->fetch_assoc();
-				$_SESSION['id'] = $wiersz['id'];
-				$_SESSION['user'] = $wiersz['user'];		
-				$_SESSION['drewno'] = $wiersz['drewno'];		
-				$_SESSION['kamien'] = $wiersz['kamien'];		
-				$_SESSION['zboze'] = $wiersz['zboze'];		
-				$_SESSION['email'] = $wiersz['amail'];		
-				$_SESSION['dnipremium'] = $wiersz['dnipremium'];		
+				
+				if (password_verify($haslo, $wiersz['pass']))
+				{
+					
+					$_SESSION['zalogowany'] = true;
+					
+					$_SESSION['id'] = $wiersz['id'];
+					$_SESSION['user'] = $wiersz['user'];		
+					$_SESSION['drewno'] = $wiersz['drewno'];		
+					$_SESSION['kamien'] = $wiersz['kamien'];		
+					$_SESSION['zboze'] = $wiersz['zboze'];		
+					$_SESSION['email'] = $wiersz['amail'];		
+					$_SESSION['dnipremium'] = $wiersz['dnipremium'];		
 
-				unset($_SESSION['blad']);
-				$rezultat->free_result();				
-				header('Location: gra.php');	
-				# przekierowanie z uzyciem naglowka HTTP
+					unset($_SESSION['blad']);
+					$rezultat->free_result();				
+					header('Location: gra.php');	
+					# przekierowanie z uzyciem naglowka HTTP
+				}
+				else 
+				{				
+				$_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
+				header('Location: index.php');
+				}
 				
 			} else {
-				
 				$_SESSION['blad'] = '<span style="color:red">Nieprawidłowy login lub hasło!</span>';
 				header('Location: index.php');
 			}
